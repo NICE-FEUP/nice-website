9c126fd61025a6d208bfd53951f37a0320e85ecf
FernandoJAlves <up201605270@fe.up.pt>
2019-09-07 23:21:34 +0100

    Navbar effect done

--- js/homepage.js
+++ js/homepage.js
@@ -1,2 +1,14 @@
+onLoad();
 
+
+function onLoad() {
+  // SETUP TRIANGLE WIDTH
+  const triangle = document.querySelector('#logo-triangle');
+  const triHeight = triangle.offsetHeight;
+  //const triProperties = triangle.getBoundingClientRect();
+
+  triangle.setAttribute('style',`width:${1.5*triHeight}px`);
+
+  
+}
 
